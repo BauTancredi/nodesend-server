eab7648bd486a094cb96e940aab9320c795000ab
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-07-31 20:31:13 -0300

    Avoiding duplicate users

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,15 @@ const User = require("../models/User");
 exports.newUser = async (req, res) => {
   console.log(req.body);
 
-  const user = await new User(req.body);
+  // Verify if user is already registered
+  const { email } = req.body;
+
+  let user = await User.findOne({ email });
+
+  if (user)
+    return res.status(400).json({ msg: "The user is already registered" });
+
+  user = await new User(req.body);
   user.save();
 
   res.json({ msg: "User created" });
