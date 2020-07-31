111d2ea1e89c7a19904d8cac48ab4c64e165bf9b
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-07-31 20:11:37 -0300

    Adding routing for users

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 // Create server
 const app = express();
 
-console.log("Starting Node Send");
+// Connect to DB
+connectDB();
 
 // PORT
 const port = process.env.PORT || 4000;
+
+// Routes
+app.use("/api/users", require("./routes/users"));
 
 // Start app
 app.listen(port, "0.0.0.0", () => {
