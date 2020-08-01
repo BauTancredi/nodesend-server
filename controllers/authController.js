f7d510d29f7d7018bcfea3914ab423cc6f2a961d
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-08-01 19:23:16 -0300

    Validating email and password of user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,27 @@
-exports.authenticateUser = async (req, res, next) => {};
+const bcrypt = require("bcrypt");
+
+const User = require("../models/User");
+
+exports.authenticateUser = async (req, res, next) => {
+  // Verify errors
+
+  // Search for the user
+  const { email, password } = req.body;
+  const user = User.findOne({ email });
+
+  if (!user) {
+    res.status(401).json({ msg: "The user not exists" });
+    return next();
+  }
+
+  // Verify password and authenticate user
+
+  if (bcrypt.compareSync(password, user.password)) {
+    // Create JWT
+  } else {
+    res.status(401).json({ msg: "Incorrect password" });
+    return next();
+  }
+};
 
 exports.userAuthenticated = async (req, res) => {};
