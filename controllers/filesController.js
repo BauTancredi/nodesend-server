8ce0aa0c6d6b2f76308678f5609e798a9002034b
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-08-02 18:52:14 -0300

    Adding Multer configuration

--- controllers/filesController.js
+++ controllers/filesController.js
@@ -1,4 +1,32 @@
-exports.uploadFile = async (req, res) => {
-  console.log(req.file);
+const multer = require("multer");
+const shortid = require("shortid");
+
+const multerConfig = {
+  limits: { fileSize: 1000000 },
+  storage: (fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, __dirname + "/../uploads");
+    },
+    filename: (req, file, cb) => {
+      const extension = file.mimetype.split("/")[1];
+      cb(null, `${shortid.generate()}.${extension}`);
+    },
+  })),
 };
+
+const upload = multer(multerConfig).single("file");
+
+exports.uploadFile = async (req, res) => {
+  upload(req, res, async (error) => {
+    console.log(req.file);
+
+    if (!error) {
+      res.json({ file: req.file.filename });
+    } else {
+      console.log(error);
+      return next();
+    }
+  });
+};
+
 exports.deleteFile = async (req, res) => {};
--- routes/files.js
+++ routes/files.js
@@ -1,14 +1,11 @@
 const express = require("express");
-const multer = require("multer");
+
 const router = express.Router();
 
 const auth = require("../middleware/auth");
 const filesController = require("../controllers/filesController");
 
-// Upload Files
-const upload = multer({ dest: "./uploads/" });
-
-router.post("/", upload.single("file"), filesController.uploadFile);
+router.post("/", filesController.uploadFile);
 router.delete("/:id", filesController.deleteFile);
 
 module.exports = router;
