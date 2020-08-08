24cf986b5c30f396b72fb1ec765fb0bb21e00f45
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-08-07 22:34:49 -0300

    Moving Access-Control-Allow-Origin

--- index.js
+++ index.js
@@ -6,6 +6,27 @@ const connectDB = require("./config/db");
 // Create server
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+
+  // authorized headers for preflight requests
+  // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+
+  app.options("*", (req, res) => {
+    // allowed XHR methods
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, PATCH, PUT, POST, DELETE, OPTIONS"
+    );
+    res.send();
+  });
+});
+
 // Connect to DB
 connectDB();
 
@@ -27,27 +48,6 @@ app.use(express.json());
 
 // Allow public folder
 app.use(express.static("uploads"));
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-
-  // authorized headers for preflight requests
-  // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-
-  app.options("*", (req, res) => {
-    // allowed XHR methods
-    res.header(
-      "Access-Control-Allow-Methods",
-      "GET, PATCH, PUT, POST, DELETE, OPTIONS"
-    );
-    res.send();
-  });
-});
 
 // Routes
 app.use("/api/users", require("./routes/users"));
