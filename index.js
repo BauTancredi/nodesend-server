6ee71db5889ab1f61c281ad243f66c91f1532acf
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-08-07 22:44:25 -0300

    Allow cors

--- index.js
+++ index.js
@@ -6,39 +6,39 @@ const connectDB = require("./config/db");
 // Create server
 const app = express();
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
 
-  // authorized headers for preflight requests
-  // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
+//   // authorized headers for preflight requests
+//   // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
 
-  app.options("*", (req, res) => {
-    // allowed XHR methods
-    res.header(
-      "Access-Control-Allow-Methods",
-      "GET, PATCH, PUT, POST, DELETE, OPTIONS"
-    );
-    res.send();
-  });
-});
+//   app.options("*", (req, res) => {
+//     // allowed XHR methods
+//     res.header(
+//       "Access-Control-Allow-Methods",
+//       "GET, PATCH, PUT, POST, DELETE, OPTIONS"
+//     );
+//     res.send();
+//   });
+// });
 
 // Connect to DB
 connectDB();
 
 // Allow CORS
-// const optionsCORS = {
-//   origin: [
-//     "http://localhost:3000",
-//     "https://admiring-jones-9b5088.netlify.app",
-//   ],
-// };
+const optionsCORS = {
+  origin: [
+    "http://localhost:3000",
+    "https://admiring-jones-9b5088.netlify.app",
+  ],
+};
 
-// app.use(cors(optionsCORS));
+app.use(cors(optionsCORS));
 
 // PORT
 const port = process.env.PORT || 4000;
