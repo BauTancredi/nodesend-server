9b697c346ba55112dbabdba8817721f5e5f51363
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-09-14 19:33:03 -0300

    deployment with udemy

--- index.js
+++ index.js
@@ -6,39 +6,15 @@ const connectDB = require("./config/db");
 // Create server
 const app = express();
 
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-
-//   // authorized headers for preflight requests
-//   // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-
-//   app.options("*", (req, res) => {
-//     // allowed XHR methods
-//     res.header(
-//       "Access-Control-Allow-Methods",
-//       "GET, PATCH, PUT, POST, DELETE, OPTIONS"
-//     );
-//     res.send();
-//   });
-// });
-
 // Connect to DB
 connectDB();
 
 // Allow CORS
-// const optionsCORS = {
-//   origin: [
-//     "http://localhost:3000",
-//     "https://admiring-jones-9b5088.netlify.app",
-//   ],
-// };
+const optionsCORS = {
+  origin: process.env.FRONTEND_URL,
+};
 
-app.use(cors());
+app.use(cors(optionsCORS));
 
 // PORT
 const port = process.env.PORT || 4000;
